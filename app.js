3d471cb07f147fcca295731fee7b2349932c593a
Ihor Leonov <ihorleonow@gmail.com>
2023-06-22 17:50:48 +0300

    fixed register/login

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
 
-const userRouter = require('./routes/api/users-routes');
+const usersRouter = require('./routes/api/users-routes');
 const contactsRouter = require('./routes/api/contacts-routes');
 
 const app = express();
@@ -13,7 +13,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/user', userRouter);
+app.use('/api/users', usersRouter);
 app.use('/api/contacts', contactsRouter);
 
 app.use((req, res) => {
--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,53 +1,19 @@
-// SendGrid way
+// const sgMail = require('@sendgrid/mail');
+// require('dotenv').config();
 
-const sgMail = require('@sendgrid/mail');
-require('dotenv').config();
-
-const { SENDGRID_API_KEY } = process.env;
-sgMail.setApiKey(SENDGRID_API_KEY);
+// const { SENDGRID_API_KEY } = process.env;
+// sgMail.setApiKey(SENDGRID_API_KEY);
 
 // const data = {
 //     to: 'vejope2488@soremap.com',
-//     from: 'pancishi1@gmail.com',
 //     subject: 'Test email',
 //     html: '<p><strong>Test email</strong> from localhost:3000</p>',
 // };
 
-const sendEmail = async data => {
-    const email = { ...data, from: 'pancishi1@gmail.com' };
-    await sgMail.send(email);
-    return true;
-};
-
-module.exports = sendEmail;
-
-// Nodemailer way - подключаемся к почтовому серверу
-
-// 1. npm i nodemailer
-// 2. add "META_PASSWORD=Pass12345" to env
-
-// const nodemailer = require('nodemailer');
-// const { META_PASSWORD } = process.env;
-
-// const nodemailerConfig = {
-//     host: 'smtp.meta.ua',
-//     port: 465, // (25, 465, 2525) - 465й требует шифрование
-//     secure: true,
-//     auth: {
-//         user: 'ihor_leonov@meta.ua',
-//         pass: META_PASSWORD,
-//     },
-// };
-// const transport = nodemailer.createTransport(nodemailerConfig);
-
-// const email = {
-//     to: 'vejope2488@soremap.com',
-//     from: 'ihor_leonov@meta.ua',
-//     subject: 'Test email',
-//     html: '<p><strong>Test email</strong> from localhost:3000</p>',
+// const sendEmail = async data => {
+//     const email = { ...data, from: 'pancishi1@gmail.com' };
+//     await sgMail.send(email);
+//     return true;
 // };
 
-// transport
-//     .sendMail(email)
-//     .then(() => console.log('Email send success'))
-//     .catch(error => console.log(error.message));
+// module.exports = sendEmail;
