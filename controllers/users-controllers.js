54248fc7c7fe5ff3b9841a12d337f580d512add4
333Nikita333 <nikitosfox333@gmail.com>
2023-06-28 01:42:15 +0300

    made a return of the token after registration

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -11,87 +11,96 @@ const { catchAsync } = require('../utils');
 const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    const avatarUrl = gravatar.url(email);
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  const avatarUrl = gravatar.url(email);
 
-    if (user) {
-        throw new HttpError(409, 'Email already in use');
-    }
+  if (user) {
+    throw new HttpError(409, 'Email already in use');
+  }
 
-    const hashPassword = await bcrypt.hash(password, 10);
+  const hashPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await User.create({
-        ...req.body,
-        password: hashPassword,
-        avatarUrl,
-    });
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarUrl,
+  });
 
-    res.status(201).json({
-        user: {
-            name: newUser.name,
-            email: newUser.email,
-            avatarUrl,
-        },
-    });
+  const payload = {
+    id: newUser._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
+  await User.findByIdAndUpdate(newUser._id, { token });
+  
+  res.status(201).json({
+    token,
+    user: {
+      name: newUser.name,
+      email: newUser.email,
+      avatarUrl,
+    },
+  });
 };
 
 const login = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-    if (!user) {
-        throw new HttpError(401, 'Email or password is wrong');
-    }
+  if (!user) {
+    throw new HttpError(401, 'Email or password is wrong');
+  }
 
-    const passwordCompare = await bcrypt.compare(password, user.password);
+  const passwordCompare = await bcrypt.compare(password, user.password);
 
-    if (!passwordCompare) {
-        throw new HttpError(401, 'Email or password is wrong');
-    }
+  if (!passwordCompare) {
+    throw new HttpError(401, 'Email or password is wrong');
+  }
 
-    const payload = {
-        id: user._id,
-    };
+  const payload = {
+    id: user._id,
+  };
 
-    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
-    await User.findByIdAndUpdate(user._id, { token });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
+  await User.findByIdAndUpdate(user._id, { token });
 
-    res.json({
-        token,
-        user: {
-            name: user.name,
-            email: user.email,
-            subscription: user.subscription,
-        },
-    });
+  res.json({
+    token,
+    user: {
+      name: user.name,
+      email: user.email,
+      subscription: user.subscription,
+      avatarUrl: user.avatarUrl
+    },
+  });
 };
 
 const getCurrent = async (req, res) => {
-    const { name, email, birthday, token, phone, skype, avatarUrl } = req.user;
+  const { name, email, birthday, token, phone, skype, avatarUrl } = req.user;
 
-    res.json({
-        status: 'success',
-        code: 200,
-        user: {
-            name,
-            birthday,
-            email,
-            phone,
-            skype,
-            avatarUrl,
-        },
-        token,
-    });
+  res.json({
+    status: 'success',
+    code: 200,
+    user: {
+      name,
+      birthday,
+      email,
+      phone,
+      skype,
+      avatarUrl,
+    },
+    token,
+  });
 };
 
 const logout = async (req, res) => {
-    const { _id } = req.user;
-    await User.findByIdAndUpdate(_id, { token: '' });
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: '' });
 
-    res.json({
-        message: 'Logout succeess',
-    });
+  res.json({
+    message: 'Logout succeess',
+  });
 };
 // const updateUserAvatar = async (req, res) => {
 //     const { _id } = req.user;
@@ -115,24 +124,24 @@ const logout = async (req, res) => {
 // };
 
 const updateProfile = catchAsync(async (req, res, next) => {
-    const { _id } = req.user;
-    if (req.file) {
-        req.body.avatarUrl = req.file.path;
-    }
-    const user = await User.findByIdAndUpdate(_id, req.body, {
-        new: true,
-    }).select('-password -updatedAt -createdAt -token');
+  const { _id } = req.user;
+  if (req.file) {
+    req.body.avatarUrl = req.file.path;
+  }
+  const user = await User.findByIdAndUpdate(_id, req.body, {
+    new: true,
+  }).select('-password -updatedAt -createdAt -token');
 
-    res.status(200).json({
-        data: user,
-    });
+  res.status(200).json({
+    data: user,
+  });
 });
 
 module.exports = {
-    register: ctrlWrapper(register),
-    login: ctrlWrapper(login),
-    getCurrent: ctrlWrapper(getCurrent),
-    logout: ctrlWrapper(logout),
-    // updateUserAvatar: ctrlWrapper(updateUserAvatar),
-    updateProfile: ctrlWrapper(updateProfile),
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
+  // updateUserAvatar: ctrlWrapper(updateUserAvatar),
+  updateProfile: ctrlWrapper(updateProfile),
 };
