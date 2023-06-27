f477102c3a6bd2b93d96032c08cc5586033170e6
Anatolij Goga <anzefal@gmail.com>
2023-06-27 22:31:18 +0300

    Minor fix tasks-controllers

--- services/tasksServices.js
+++ services/tasksServices.js
@@ -2,78 +2,76 @@ const { Task } = require('../models/task');
 const { HttpError } = require('../helpers');
 
 const show = async (owner, filter) => {
-    const tasks = await Task.find(
-        {
-            owner,
-            date: { $regex: filter, $options: 'i' },
-        },
-        '-createdAt -updatedAt -__v'
-    );
-    if (!tasks) {
-        throw HttpError(400, 'Unable to fetch tasks');
-    }
+  const tasks = await modelTask.find(
+    { owner, date: { $regex: filter, $options: 'i' } },
+    '-createdAt -updatedAt -__v'
+  );
 
-    return tasks;
+  if (!tasks) {
+    throw HttpError(400, 'Unable to fetch Tasks');
+  }
+
+  return tasks;
 };
 
 const showById = async id => {
-    const task = await Task.findById(id, '-createdAt -updatedAt -__v');
+  const task = await Task.findById(id, '-createdAt -updatedAt -__v');
 
-    if (!task) {
-        throw HttpError(400, 'Unable to find task');
-    }
+  if (!task) {
+    throw HttpError(400, 'Unable to find task');
+  }
 
-    return task;
+  return task;
 };
 
 const add = async (owner, data) => {
-    const tasks = await Task.create({ ...data, owner });
+  const tasks = await Task.create({ ...data, owner });
 
-    if (!tasks) {
-        throw HttpError(400, 'Unable to save to the database');
-    }
+  if (!tasks) {
+    throw HttpError(400, 'Unable to save to the database');
+  }
 
-    const { createdAt, updatedAt, __v, ...createdTask } = tasks.toObject();
+  const { createdAt, updatedAt, __v, ...createdTask } = tasks.toObject();
 
-    return createdTask;
+  return createdTask;
 };
 
 const change = async (id, data) => {
-    const task = await Task.findByIdAndUpdate(id, data, {
-        new: true,
-        select: '-createdAt -updatedAt -__v',
-    });
+  const task = await Task.findByIdAndUpdate(id, data, {
+    new: true,
+    select: '-createdAt -updatedAt -__v',
+  });
 
-    if (!task) {
-        throw HttpError(400, 'Unable to find task');
-    }
+  if (!task) {
+    throw HttpError(400, 'Unable to find task');
+  }
 
-    return task;
+  return task;
 };
 
 const changeCategory = async (id, data) => {
-    const task = await Task.findByIdAndUpdate(id, data, {
-        new: true,
-        select: '-createdAt -updatedAt -__v',
-    });
+  const task = await Task.findByIdAndUpdate(id, data, {
+    new: true,
+    select: '-createdAt -updatedAt -__v',
+  });
 
-    if (!task) {
-        throw HttpError(400, 'Unable to find task');
-    }
+  if (!task) {
+    throw HttpError(400, 'Unable to find task');
+  }
 
-    return task;
+  return task;
 };
 
 const remove = async id => {
-    const task = await Task.findByIdAndDelete(id, {
-        select: '-createdAt -updatedAt -__v',
-    });
+  const task = await Task.findByIdAndDelete(id, {
+    select: '-createdAt -updatedAt -__v',
+  });
 
-    if (!task) {
-        throw HttpError(400, 'Unable to find task');
-    }
+  if (!task) {
+    throw HttpError(400, 'Unable to find task');
+  }
 
-    return task;
+  return task;
 };
 
 module.exports = { show, showById, add, change, changeCategory, remove };
