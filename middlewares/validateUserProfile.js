a1ff3c0213dcf8e260d86209d16eb2f9d5b88114
Ihor Leonov <ihorleonow@gmail.com>
2023-07-05 00:30:07 +0300

    Update validateUserProfile.js

--- middlewares/validateUserProfile.js
+++ middlewares/validateUserProfile.js
@@ -1,15 +1,12 @@
 const { HttpError } = require('../helpers');
 const { schemas } = require('../schemas/user-schema');
-const { catchAsync } = require('../utils');
 
-const validateUserProfile = catchAsync(async (req, res, next) => {
+const validateUserProfile = async (req, res, next) => {
   const { error, value } = schemas.updateUserProfileSchema(req.body);
 
   if (error) return next(new HttpError(400, error.details[0].message));
-
   req.body = value;
-
   next();
-});
+};
 
 module.exports = validateUserProfile;
